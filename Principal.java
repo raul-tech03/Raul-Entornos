5887b5fb85407bc2df4d22d84c841de724a0cb9c
raul-tech03 <rtc2007murcia@gmail.com>
2026-02-05 12:48:52 +0100

    Updated Principal.java

--- Principal.java
+++ Principal.java
@@ -1,6 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class Principal{
     public static void main(String [] args){
         
         System.out.print("Hola mundo");
+    }
+
+    public static String fechaHora() {
+        LocalDateTime present = LocarDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.offPattern("dd/MM/yyyy HH:mm:ss");
+
+    
+        return present.format(format);
+    
     }
 }
